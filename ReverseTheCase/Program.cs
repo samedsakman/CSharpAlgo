77d969a12ab5dcb92d9805e8a115e689018821de
Samed Sakman <samedya@gmail.com>
2022-04-29 16:39:17 +0300

    First

---
+++ ReverseTheCase/Program.cs
@@ -0,0 +1,22 @@
+﻿ static string ReverseCase(string str) 
+    {
+			string newStr = "";
+			
+			for (int i = 0; i < str.Length; i++)
+			{
+				if(Convert.ToInt32(str[i]) >= 97 && Convert.ToInt32(str[i]) <= 122)
+				{
+					newStr += Convert.ToChar(Convert.ToInt32(str[i]) - 32);
+				} else if (Convert.ToInt32(str[i]) >= 65 && Convert.ToInt32(str[i]) <= 90)
+				{
+					newStr += Convert.ToChar(Convert.ToInt32(str[i]) + 32);
+				} else
+				{
+					newStr += str[i];
+				}
+			}
+			
+			return newStr;
+
+    }
+Console.WriteLine(ReverseCase("heLLo World"));

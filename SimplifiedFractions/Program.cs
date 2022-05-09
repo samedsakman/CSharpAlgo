6bdb545669882ccb8aa8a5d62a4e711c32d1a2b6
Samed Sakman <samedya@gmail.com>
2022-05-09 15:38:53 +0300

    Update

---
+++ SimplifiedFractions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+// Create a function that returns the simplified version of a fraction.
+
+string Simplify (int num1, int num2)
+{
+    if(num1 % num2 == 0)
+        return $"Simplified Fractions: {num1 / num2}";
+    else
+    {
+        for(int i = 1; i <= Math.Max(num1, num2); )
+        {
+            while(num1 % i == 0 && num2 % i == 0)
+            {
+                num1 /= i;
+                num2 /= i;
+                break;
+            }
+
+            i++;
+        }
+        return $"Simplified Fractions: {num1}/{num2}";
+    }
+}
+
+Console.WriteLine("Input 2 numbers:");
+Console.WriteLine(Simplify(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));

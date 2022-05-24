311cd5cbaf8a157ace8f86c1aa082e46d6c4efe0
Samed Sakman <samedya@gmail.com>
2022-05-25 01:15:33 +0300

    W11

--- FootballGame/Teams/Manager.cs
+++ FootballGame/Teams/Manager.cs
@@ -10,10 +10,18 @@ namespace FootballGame.Teams
     {
         public Manager(int teamId) : base(teamId)
         {
+            if (teamId == 1)
+                ReputationMultiplier = 0.2;
+            else if (teamId == 2)
+                ReputationMultiplier = 0.3;
+            else if (teamId == 3)
+                ReputationMultiplier = 0.1;
+            else
+                ReputationMultiplier = 0.1;
            
         }
 
         public string ManagerName { get; set; }
-        public int ReputationMultiplier { get; set; }
+        public double ReputationMultiplier { get; set; }
     }
 }
